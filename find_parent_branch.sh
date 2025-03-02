52e11f298a983b60570eb5d1143c01afd5a89e16
tettuan <tettuan@gmail.com>
2025-03-02 19:44:27 +0900

    Add script to find parent branch
    This script identifies the parent branch from which the current branch was derived.
    It outputs only the branch name for easy use in other scripts or commands.

---
+++ find_parent_branch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# このスクリプトは現在のブランチが派生された元のブランチを特定します
+# 使用方法: ./find_parent_branch.sh
+
+git show-branch | grep '*' | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -1 | awk -F'[]~^[]' '{print $2}' 
